676675ef79461ca37a353cc78aa9ab841caf039c
Alfredo Suarez <31414624+alfredosa@users.noreply.github.com>
2023-06-14 20:52:42 +0000

    latest status

--- projects/airflow/dags/dbt_orch.py
+++ projects/airflow/dags/dbt_orch.py
@@ -1,6 +1,6 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from datetime import datetime
+from datetime import datetime, timedelta
 
 default_args = {
     'owner': 'your_name',
---
+++ projects/postgres/docker_postgres_init.sql
@@ -0,0 +1,9 @@
+CREATE USER admin WITH PASSWORD 'admin' CREATEDB;
+CREATE DATABASE airflow
+    WITH
+    OWNER = admin
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
