1142beecc12508cceae13f38656537059d6bb721
alfredo-ben <alfredo@thanksben.com>
2023-06-15 19:36:23 +0200

    Adding dbt top pokemon models, enhanced python dag

--- projects/airflow/dags/dbt_dag.py
+++ projects/airflow/dags/dbt_dag.py
@@ -3,16 +3,17 @@ from pendulum import datetime
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from cosmos.providers.dbt.task_group import DbtTaskGroup
-
+from cosmos.providers.dbt import DbtDag
 
 with DAG(
-    dag_id="extract_dag",
+    dag_id="onestack_dbt_dag",
     start_date=datetime(2022, 11, 27),
-    schedule="@daily",
+    schedule=None,
 ):
     e1 = EmptyOperator(task_id="pre_dbt")
 
     dbt_tg = DbtTaskGroup(
+        dbt_root_path='/opt/airflow/dbt',
         dbt_project_name="onestack",
         conn_id="postgres",
         profile_args={
