16f1a2b4605871bae2ec7a99487bf64928fb202a
alfredo-ben <alfredo@thanksben.com>
2023-06-16 11:31:08 +0200

    starting separation with airflow and dagster

--- projects/postgres/docker_postgres_init.sql
+++ projects/postgres/docker_postgres_init.sql
@@ -1,3 +1,4 @@
+/* We use this file to create a separate db, for airflow, so that we can have all neatly spinup */
 CREATE USER admin WITH PASSWORD 'admin' CREATEDB;
 CREATE DATABASE airflow
     WITH
